99d4710909c6da401ee0786c53750aa5cf1c9a31
Fernando Ospina <ferchoaog@hotmail.com>
2018-05-21 20:41:56 -0500

    Agregar archivos de proyecto.

---
+++ FactoryMethod/Configuracion.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class FileManagement
+    {
+
+        public FileManagement()
+        {
+
+        }
+        public void SaveFile(string nombreArchivo, string datos)
+        {
+            string fileName = Path.Combine(Environment.CurrentDirectory, nombreArchivo);
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.CreateNew);
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.Write(datos);
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
+            }
+        }
+        
+
+    }
+    public class  Configuracion
+    {
+        public string TipoGrafo { get; set; }
+        public string TipoPeso { get; set; }
+        public string TipoBusqueda { get; set; }
+
+        public Configuracion()
+        {
+            TipoGrafo = string.Empty;
+            TipoPeso = string.Empty;
+            TipoBusqueda = string.Empty;
+        }
+        public void ArmarConfiguracion(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "";
+                    break;
+                case 2:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "";
+                    break;
+                case 3:
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "";
+                    break;
+                case 4:
+
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "";
+                    break;
+                case 5:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "DFS";
+                    break;
+                case 6:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "DFS";
+                    break;
+                case 7:
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "DFS";
+                    break;
+                case 8:
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "DFS";
+                    break;
+                case 9:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "BFS";
+                    break;
+                case 10:
+                     TipoGrafo = "Directed";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "BFS";
+                    break;
+                case 11:
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Unweighted";
+                     TipoBusqueda = "BFS";
+                    break;
+                case 12:
+                     TipoGrafo = "Undirected";
+                     TipoPeso = "Weighted";
+                     TipoBusqueda = "BFS";
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida para generar configuracion");
+                    break;
+            }
+        }
+
+        public int DisplayMenu()
+        {
+
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("Escoja el Tipo de Configuracion a Ejecutar - Creador de Grafos");
+            Console.WriteLine();
+            Console.WriteLine("1. Directed - Unweighted");
+            Console.WriteLine("2. Directed - Weighted");
+            Console.WriteLine("3. Undirected - Unweighted");
+            Console.WriteLine("4. Undirected - Weighted");
+            Console.WriteLine("5. Directed - Unweighted - DFS");
+            Console.WriteLine("6. Directed - Weighted - DFS");
+            Console.WriteLine("7. Undirected - Unweighted - DFS");
+            Console.WriteLine("8. Undirected - Weighted - DFS");
+            Console.WriteLine("9. Directed - Unweighted - BFS");
+            Console.WriteLine("10. Directed - Weighted - BFS");
+            Console.WriteLine("11. Undirected - Unweighted - BFS");
+            Console.WriteLine("12. Undirected - Weighted - BFS");
+
+            Console.WriteLine("99. Salir");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace  FactoryMethod
+
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            int userInput = 0;
+            
+            Configuracion configuracion = new Configuracion();
+            userInput = configuracion.DisplayMenu();
+            configuracion.ArmarConfiguracion(userInput);
+
+            //CREACION DEL GRAFO
+            CreadordeGrafo creadorGrafo;
+
+            switch (configuracion.TipoGrafo)
+            {
+                case "Directed":
+                    creadorGrafo = new CrearGrafoDirected();
+                    break;
+                case "Undirected":
+                    creadorGrafo = new CrearGrafoIndirected();
+                    break;
+                default:
+                    creadorGrafo = new CrearGrafoDirected();
+                    break;
+            }
+
+            Grafo grafo = creadorGrafo.MetodoFabricacionGrafo();
+            Console.WriteLine("Creado el tipo de Grafo {0}", grafo.GetType().Name);
+
+            //SELECCION Y CALCULO DEL PESO SI VIENE EN LA CONFIGURACION
+            if (configuracion.TipoPeso.Equals("Weighted"))
+            {
+                DecaradorConcretoCalcularPeso decoradorCalculoPeso = new DecaradorConcretoCalcularPeso();
+                decoradorCalculoPeso.SetComponent(grafo);
+                decoradorCalculoPeso.CalcularPeso();
+            }
+            else
+            {
+                Console.WriteLine("La configuracion no tiene un calculo de peso definido");
+            }
+
+            //SELECCION Y TIPO DE ESTRATEGIA SI VIENE EN LA CONFIGURACION
+            ContextodeCalculo contextodeCalculo;
+
+            switch (configuracion.TipoBusqueda)
+            {
+                case "BFS":
+                    contextodeCalculo = new ContextodeCalculo(new EstrategiaBFS());
+                    contextodeCalculo.InterfacedeContexto(grafo);
+                    break;
+                case "DFS":
+                    contextodeCalculo = new ContextodeCalculo(new EstrategiaDFS());
+                    contextodeCalculo.InterfacedeContexto(grafo);
+                    break;
+                default:
+                    Console.WriteLine("La configuracion no tiene una estrategia de busqueda definida");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+
+    //CLASES DEL METODO DE FABRICA CONSTRUCCION GRAFO
+    abstract class Grafo : ComponenteCalculoPeso
+    {
+    }
+
+    class GrafoConcretoDirected : Grafo 
+    {
+        public override void CalcularPeso()
+        {
+            Console.WriteLine("\t\tSe realizo operacion base de Calculo de Peso en Grafo Dirigido");
+        }
+    }
+    class GrafoConcretoIndirected : Grafo
+    {
+        public override void CalcularPeso()
+        {
+            Console.WriteLine("\t\tSe realizo operacion base de Calculo de Peso en Grafo No Dirigido");
+        }
+    }
+    abstract class CreadordeGrafo
+    {
+        public abstract Grafo MetodoFabricacionGrafo();
+    }
+    class CrearGrafoDirected : CreadordeGrafo
+    {
+        public override Grafo MetodoFabricacionGrafo()
+        {
+            return new GrafoConcretoDirected();
+        }
+    }
+    class CrearGrafoIndirected : CreadordeGrafo
+    {
+        public override Grafo MetodoFabricacionGrafo()
+        {
+            return new GrafoConcretoIndirected();
+        }
+    }
+
+    // CLASES DEL DECORADOR PARA EL CALCULO DE PESO
+    abstract class ComponenteCalculoPeso
+
+    {
+        public abstract void CalcularPeso();
+    }
+    class ComponenteConcreto : ComponenteCalculoPeso
+
+    {
+        public override void CalcularPeso()
+        {
+            Console.WriteLine("\t\tOperacion Indicada en el Componente de Calculo Concreto");
+        }
+    }
+    abstract class Decorador : ComponenteCalculoPeso
+
+    {
+        protected ComponenteCalculoPeso componenteCalcularPeso;
+
+        public void SetComponent(ComponenteCalculoPeso component)
+        {
+            this.componenteCalcularPeso = component;
+        }
+
+        public override void CalcularPeso()
+        {
+            if (componenteCalcularPeso != null)
+            {
+                componenteCalcularPeso.CalcularPeso();
+            }
+        }
+    }
+    class DecaradorConcretoCalcularPeso : Decorador
+
+    {
+        public override void CalcularPeso()
+        {
+            base.CalcularPeso();
+            Console.WriteLine("\t\t Se realizo la operacion sobre el Decorador CalcularPeso()");
+        }
+    }
+
+    // CLASES DE ESTRATEGIA PARA BUSQUEDA DEL ARBOL A LO ANCHO Y POR PROFUNDIDAD
+
+    abstract class EstrategiadeCalculo
+
+    {
+        public abstract void AlgoritmodeCalculo(Grafo grafo);
+    }
+    class EstrategiaBFS : EstrategiadeCalculo
+
+    {
+        public override void AlgoritmodeCalculo(Grafo grafo)
+        {
+            Console.WriteLine(
+              "Llamado a la EstrategiaBFS.AlgoritmodeCalculo()");
+        }
+    }
+    class EstrategiaDFS : EstrategiadeCalculo
+
+    {
+        public override void AlgoritmodeCalculo(Grafo grafo)
+        {
+            Console.WriteLine(
+              "Llamado a la EstrategiaDFS.AlgoritmodeCalculo()");
+        }
+    }
+    class ContextodeCalculo
+
+    {
+        private EstrategiadeCalculo _Estrategia;
+
+        public ContextodeCalculo(EstrategiadeCalculo estrategiadeCalculo)
+        {
+            this._Estrategia = estrategiadeCalculo;
+        }
+
+        public void InterfacedeContexto(Grafo grafo)
+        {
+            _Estrategia.AlgoritmodeCalculo(grafo);
+        }
+    }
+}
+
